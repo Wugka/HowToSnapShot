7bcd078f92f77b9ee79a024bf2f739321e092021
Wugka <61409654+Wugka@users.noreply.github.com>
2021-11-22 12:00:52 +0700

    Create PrivateRoute.js

---
+++ PrivateRoute.js
@@ -0,0 +1,31 @@
+// This is used to determine if a user is authenticated and
+// if they are allowed to visit the page they navigated to.
+
+// If they are: they proceed to the page
+// If not: they are redirected to the login page.
+
+import React from 'react'
+import AuthenServices from '../services/authenServices'
+import { Redirect, Route } from 'react-router-dom'
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+    // Add your own authentication on the below line.
+    const auth = new AuthenServices()
+    const isLogged = auth.loggedIn()
+
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                isLogged ? (
+                    <Component {...props} />
+                ) : (
+                        <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+                    )
+            }
+        />
+    )
+}
+
+export default PrivateRoute
